e1b30e6593f9acb1e0375c837599d702aabca1f1
williamschen23 <chenwill2005@gmail.com>
2024-11-09 22:59:17 -0500

    finished autocomplete

--- src/StateInformation.tsx
+++ src/StateInformation.tsx
@@ -1,15 +1,26 @@
-import { Grid2, Typography, Button } from '@mui/material';
+import { Grid2, Typography, Button, Container } from '@mui/material';
 import { StateProp } from './models/StateProp'
+import PlacesAutocomplete from './components/PlacesAutocomplete';
+import { useState } from 'react';
+import { LoadScript } from '@react-google-maps/api';
 
 export default function StateInformation(props: StateProp) {
+	const [place1, setPlace1] = useState('');
+	const [place2, setPlace2] = useState('');
+
   return (
 		<Grid2 container spacing={2} direction={"row"}>
 			{/* Autocomplete Container */}
 			<Grid2 size={6} justifyContent={"center"} alignItems={"center"} display={"flex"}>
-				<Typography>From:</Typography>
-				<br></br>
-				<Typography>To:</Typography>
-				<Button>Submit</Button>
+				<Container>
+					<LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_API_KEY} libraries={['places']}>
+						<Typography>From:</Typography>
+						<PlacesAutocomplete selectedPlace={place1} setSelectedPlace={setPlace1}></PlacesAutocomplete>
+						<Typography>To:</Typography>
+						<PlacesAutocomplete selectedPlace={place2} setSelectedPlace={setPlace2}></PlacesAutocomplete>
+						<Button>Submit</Button>
+					</LoadScript>
+				</Container>
 			</Grid2>
 
 			{/* Heatmap of Crime */}
---
+++ src/models/PlacesProp.tsx
@@ -0,0 +1,4 @@
+export interface PlacesProp {
+    selectedPlace: string
+    setSelectedPlace: React.Dispatch<React.SetStateAction<string>>
+}
