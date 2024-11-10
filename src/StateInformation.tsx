f5a5363d03c9eb23aae37b696fb4d59258819998
kaeshev <kaeshev@gmail.c0om>
2024-11-10 04:51:49 -0500

    snackbar, alert, star rating

--- src/StateInformation.tsx
+++ src/StateInformation.tsx
@@ -1,6 +1,6 @@
-import {Grid2, Typography, Box, Container, Fab, Stack, Select, MenuItem} from '@mui/material';
+import {Grid2, Typography, Box, Container, Fab, Stack, Select, MenuItem, Rating, Alert, Snackbar} from '@mui/material';
 import PlacesAutocomplete from './components/PlacesAutocomplete';
-import {Dispatch, SetStateAction} from 'react';
+import {Dispatch, SetStateAction, useEffect, useState} from 'react';
 import MapComponent from "./heatmap.tsx";
 
 
@@ -18,14 +18,18 @@ interface StateInformationProp {
 }
 
 export default function StateInformation(props: StateInformationProp) {
+	const [starRating, setStarRating] = useState<number | null>(0);
+	const [alert, setAlert] = useState<boolean>(false)
 
+	useEffect(() => {setStarRating(0)}, [alert])
   return (
 		<Grid2 container spacing={3} direction={"column"}>
 			{/* Autocomplete Container */}
 			<Grid2 justifyContent={"center"} alignItems={"center"} display={"flex"}>
 				<Container>
 					<Stack spacing={2}>
-						<Select defaultValue={"walking"} onChange={(event) => {props.setMobilityType(event.target.value as string)}}>
+						<Select defaultValue={"walking"} onChange={(event) => {props.setMobilityType(event.target.value as string);
+							setAlert(false)}}>
 							<MenuItem value={"walking"}>Walking</MenuItem>
 							<MenuItem value={"driving"}>Driving</MenuItem>
 						</Select>
@@ -33,7 +37,30 @@ export default function StateInformation(props: StateInformationProp) {
 						<PlacesAutocomplete selectedPlace={props.placeTo} setSelectedPlace={props.setPlaceTo} location={'To:'}></PlacesAutocomplete>
 						<Fab onClick= {()=>{
 							props.setSearchButton(props.searchButton+1);
-						}}sx={{ bgcolor: (props.placeFrom ==='' || props.placeTo ==='') ? '' : "#9ac8d9" }} variant={"extended"} ><Box fontWeight='fontWeightMedium'><Typography variant={"body1"} >Submit</Typography></Box></Fab>
+							setAlert(false)
+						}}sx={{ bgcolor: (props.placeFrom ==='' || props.placeTo ==='') ? '' : "#9ac8d9" }} variant={"extended"} ><Box fontWeight='fontWeightMedium'><Typography variant={"body1"} >Find a route!</Typography></Box></Fab>
+						<Typography>Review your route here:</Typography>
+						{props.searchButton >= 0 && <Rating
+							name="simple-controlled"
+							value={starRating}
+							defaultValue={0}
+							onClick = {() => {setAlert(true)
+							}}
+							onChange={(event, newValue) => {
+								setStarRating(newValue);
+							}}
+						/>}
+
+						{alert &&
+							<Snackbar
+								anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+								style={{
+									backgroundColor:'#83bacc',
+								}}
+								open={alert}
+								message="Your route rating has been added to our database!"
+							>
+							</Snackbar>}
 					</Stack>
 				</Container>
 			</Grid2>
